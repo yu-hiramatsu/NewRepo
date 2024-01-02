8da24ca7c0ff8edd9098bc93bc0c78935eea3528
sam <sam@DESKTOP-DN3PQ9R>
2024-01-03 08:29:10 +0900

    Add project files.

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+
+
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            writeDateToolStripMenuItem = new ToolStripMenuItem();
+            panel1 = new Panel();
+            listBox1 = new ListBox();
+            pictureBox1 = new PictureBox();
+            menuStrip1.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { openToolStripMenuItem, writeDateToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(476, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(46, 20);
+            openToolStripMenuItem.Text = "open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // writeDateToolStripMenuItem
+            // 
+            writeDateToolStripMenuItem.Name = "writeDateToolStripMenuItem";
+            writeDateToolStripMenuItem.Size = new Size(71, 20);
+            writeDateToolStripMenuItem.Text = "write date";
+            writeDateToolStripMenuItem.Click += writeDateToolStripMenuItem_Click;
+            // 
+            // panel1
+            // 
+            panel1.AutoScroll = true;
+            panel1.Controls.Add(listBox1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 24);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(476, 357);
+            panel1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            listBox1.Dock = DockStyle.Right;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(341, 0);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(135, 357);
+            listBox1.TabIndex = 1;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Left;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(339, 357);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(476, 381);
+            Controls.Add(panel1);
+            Controls.Add(menuStrip1);
+            KeyPreview = true;
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem writeDateToolStripMenuItem;
+        private ListBox listBox1;
+    }
+}
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,131 @@
+﻿
+using System.Drawing.Imaging;
+using System.Text;
+using System.Windows.Forms;
+using ExifLib;
+
+
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var bmp = ImageFileOpen();
+            pictureBox1.Image = bmp;
+        }
+
+        private Bitmap ImageFileOpen()
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Image File(*.bmp,*.jpg,*.png,*.tif)|*.bmp;*.jpg;*.png;*.tif|Bitmap(*.bmp)|*.bmp|Jpeg(*.jpg)|*.jpg|PNG(*.png)|*.png";
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == DialogResult.Cancel) return null;
+            return ImageFileOpen(ofd.FileName);
+        }
+
+        private Bitmap ImageFileOpen(string fileName)
+        {
+            // 指定したファイルが存在するか？確認
+            if (System.IO.File.Exists(fileName) == false) return null;
+
+            // 拡張子の確認
+            var ext = System.IO.Path.GetExtension(fileName).ToLower();
+
+            // ファイルの拡張子が対応しているファイルかどうか調べる
+            if (
+            (ext != ".bmp") &&
+            (ext != ".jpg") &&
+            (ext != ".png") &&
+            (ext != ".tif")
+            )
+            {
+                return null;
+            }
+
+            Bitmap bmp;
+
+
+            // ファイルストリームでファイルを開く
+            using (var fs = new System.IO.FileStream(
+            fileName,
+            System.IO.FileMode.Open,
+            System.IO.FileAccess.Read))
+            {
+                bmp = new Bitmap(fs);
+                string pathvar = fs.Name;
+
+                foreach (System.Drawing.Imaging.PropertyItem item in bmp.PropertyItems)
+                {
+                    if (item.Type == 2)
+                    {
+                        //ASCII文字の場合は、文字列に変換する
+                        string val = System.Text.Encoding.ASCII.GetString(item.Value);
+                        val = val.Trim(new char[] { '\0' });
+                        int len = val.Length;
+                        if (len >= 15)
+                        {
+                            //表示する
+                            //System.Windows.Forms.MessageBox.Show($"{item.Id:132}: {item.Type}: {val}");
+                            listBox1.Items.Add($"{val}");
+                            break;
+                        }
+                    }
+
+
+                }
+
+            }
+
+            return bmp;
+
+        }
+
+
+
+
+
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //［Ctrl］+［Shift］+［T］が押されたらキャッチする
+            if (e.KeyData == (Keys.Control | Keys.Oemplus))
+            {
+                System.Windows.Forms.MessageBox.Show("Ctrl + ");
+            }
+            //［Ctrl］+［T］が押されたらキャッチする
+            if (e.KeyData == (Keys.Control | Keys.OemMinus))
+            {
+                System.Windows.Forms.MessageBox.Show("Ctrl -");
+            }
+        }
+
+
+
+        private void writeDateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+
+
+
+
+}
+
+
+
